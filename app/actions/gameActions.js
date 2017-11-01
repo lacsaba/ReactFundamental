1ada6cb345723e360454014a3d3c4ae30d5b4409
Csaba Lakatos <lacsaba@gmail.com>
2017-11-01 10:54:39 +0100

    minor refactor

--- app/actions/gameActions.js
+++ app/actions/gameActions.js
@@ -1,46 +1,32 @@
 import * as types from './actionTypes';
 
-export function selectNumber(selectedNumber) {
-  return {
-    type: types.SELECT_NUMBER,
-    selectedNumber,
-  };
-}
+export const selectNumber = selectedNumber => ({
+  type: types.SELECT_NUMBER,
+  selectedNumber
+});
 
-export function unselectNumber(selectedNumber) {
-  return {
-    type: types.UNSELECT_NUMBER,
-    selectedNumber,
-  };
-}
+export const unselectNumber = selectedNumber => ({
+  type: types.UNSELECT_NUMBER,
+  selectedNumber
+});
 
-export function processAnswer() {
-  return {
-    type: types.PROCESS_ANSWER,
-  };
-}
+export const processAnswer = () => ({
+  type: types.PROCESS_ANSWER
+});
 
-export function setRandomNumberOfStars(randomNumberOfStars) {
-  return {
-    type: types.SET_RANDOM_NUMBER_OF_STARS,
-    randomNumberOfStars,
-  };
-}
+export const setRandomNumberOfStars = randomNumberOfStars => ({
+  type: types.SET_RANDOM_NUMBER_OF_STARS,
+  randomNumberOfStars
+});
 
-export function removeSelectedNumbers() {
-  return {
-    type: types.REMOVE_SELECTED_NUMBERS,
-  };
-}
+export const removeSelectedNumbers = () => ({
+  type: types.REMOVE_SELECTED_NUMBERS
+});
 
-export function resetGame() {
-  return {
-    type: types.RESET_GAME,
-  };
-}
+export const resetGame = () => ({
+  type: types.RESET_GAME
+});
 
-export function redraw() {
-  return {
-    type: types.REDRAW,
-  };
-}
+export const redraw = () => ({
+  type: types.REDRAW
+});
